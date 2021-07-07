78232833d0833ce0161aa84864b92c38e7138934
Zahi Farah <zahi.farah@protonmail.com>
2021-07-07 18:30:48 +0300

    Implement delete method via Mongoose on all stored review references inside of the reviews array of it's parent Campground, rso all references are deleted upon deletion of parent Campground.

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 // include in module
 const mongoose = require("mongoose");
+const Review = require("./review");
 
 // schema
 const CampgroundSchema = new mongoose.Schema({
@@ -14,6 +15,18 @@ const CampgroundSchema = new mongoose.Schema({
             ref: "Review"
         }
     ]
+});
+
+// Post Hook Middleware (when this runs the document has already been deleted)
+// but that's fine as it's passed in to the hook middleware and we have access to it.
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: { // delete all reviews where the "_id" field exists inside the doc
+                $in: doc.reviews // we just deleted, inside it's reviews array.
+            }
+        })
+    }
 });
 
 // compile and export model
