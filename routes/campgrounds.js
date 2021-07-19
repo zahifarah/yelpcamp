30f29d011cbdbeffb2e38916187369ab24080211
Zahi Farah <zahi.farah@protonmail.com>
2021-07-19 11:16:49 +0300

    call req.flash with keyword 'success' and custom message on new campground creation

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -31,6 +31,7 @@ router.get("/new", (req, res) => {
 router.post("/", validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground); // returns {"campground: {"title: "Some Title", "location": "Some location"}"}
     const added = await campground.save();
+    req.flash("success", "New Campground successfully created!");
     console.log(`ADDED: ${added}`);
     res.redirect(`/campgrounds/${campground._id}`);
 }));
