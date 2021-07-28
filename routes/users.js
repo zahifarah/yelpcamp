7789f286d723dbdc22967381036b4469a00ae570
Zahi Farah <zahi.farah@protonmail.com>
2021-07-28 14:39:52 +0300

    User /register POST route logic implemented.

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,12 @@ router.get("/register", (req, res) => {
 });
 
 router.post("/register", async (req, res) => {
-    res.send(req.body);
+    const { username, password, email } = req.body;
+    const user = new User({ username, email });
+    const registeredUser = await User.register(user, password);
+    console.log(registeredUser);
+    req.flash("success", "Welcome to Yelp Camp!");
+    res.redirect("/campgrounds");
 });
 
 module.exports = router;
