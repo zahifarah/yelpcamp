0f9c9a20f0d2c101a6c5c08b6bda23b246e57e96
Zahi Farah <zahi.farah@protonmail.com>
2021-07-28 15:00:49 +0300

    Implement users /register POST route logic. Add a custom try/catch in addition to catchAsync()

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user")
+const catchAsync = require("../utils/catchAsync");
 
 router.get("/register", (req, res) => {
     res.render("users/register");
 });
 
-router.post("/register", async (req, res) => {
-    const { username, password, email } = req.body;
-    const user = new User({ username, email });
-    const registeredUser = await User.register(user, password);
-    console.log(registeredUser);
-    req.flash("success", "Welcome to Yelp Camp!");
-    res.redirect("/campgrounds");
-});
+router.post("/register", catchAsync(async (req, res) => {
+    try { // where errors are likely to happen
+        const { username, password, email } = req.body;
+        const user = new User({ username, email });
+        const registeredUser = await User.register(user, password);
+        console.log(registeredUser);
+        req.flash("success", "Welcome to Yelp Camp!");
+        res.redirect("/campgrounds");
+    }
+    catch (e) { // catch, flash and redirect
+        req.flash("error", e.message);
+        res.redirect("register");
+    }
+}));
 
 module.exports = router;
