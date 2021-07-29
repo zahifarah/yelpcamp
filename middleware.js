8dbe80b975bcc46bc9c2a89ffc5a7305421a8b39
Zahi Farah <zahi.farah@protonmail.com>
2021-07-29 13:18:10 +0300

    Independent file containing isLoggedIn() middleware. Checks if req object isAuthenticated before allowing route access. Flashes an error message if not.

---
+++ middleware.js
@@ -0,0 +1,8 @@
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.flash("error", "You must be signed-in to do that");
+        res.redirect("/login");
+    }
+    next();
+};
+
