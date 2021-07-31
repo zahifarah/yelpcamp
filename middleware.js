6d6c274f0632a699a590e0cc982139c80a801118
Zahi Farah <zahi.farah@protonmail.com>
2021-07-31 13:49:30 +0300

    Create login middleware that (1) will check if a user is logged in before granting access to certain URLs and (B) will remember originalUrl to be able to redirect to where the User was prior to being prompted to login.

--- middleware.js
+++ middleware.js
@@ -1,7 +1,10 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
-        req.flash("error", "You must be signed-in to do that");
-        res.redirect("/login");
+        req.session.returnTo = req.originalUrl; // stores the originalUrl in session
+        req.flash("error", "You must login first!");
+        return res.redirect("/login"); /** without the return keyword the function
+        would keep running, calling next after sending a response. In an HTTP request
+        that doesn't work, you can only send a response once and once only. */
     }
     next();
 };
