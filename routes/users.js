053a3c32124e5d174deb06121ec63e1292c25735
Zahi Farah <zahi.farah@protonmail.com>
2021-07-31 13:48:18 +0300

    Using Passport helper methods implement User register post route logic.

--- routes/users.js
+++ routes/users.js
@@ -12,8 +12,11 @@ router.get("/register", (req, res) => {
 router.post("/register", catchAsync(async (req, res) => {
     try { // where errors are likely to happen
         const { username, password, email } = req.body;
-        const user = new User({ username, email });
-        const registeredUser = await User.register(user, password);
+        const user = new User({ username, email }); // create new user
+        const registeredUser = await User.register(user, password); // register new user
+        req.login(registeredUser, err => { // login user (requires a callback, cannot await it)
+            if (err) return next(err);
+        });
         console.log(registeredUser);
         req.flash("success", "Welcome to Yelp Camp!");
         res.redirect("/campgrounds");
@@ -21,7 +24,7 @@ router.post("/register", catchAsync(async (req, res) => {
     catch (error) { // catch, flash and redirect
         req.flash("error", error.message);
         res.redirect("register");
-    }
+    };
 }));
 
 // LOGIN
@@ -30,7 +33,14 @@ router.get("/login", (req, res) => {
 });
 
 router.post("/login", passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), (req, res) => {
+    const redirectUrl = req.session.returnTo || "/campgrounds";
     req.flash("success", "Login successful! Welcome back :)");
+    res.redirect(`${redirectUrl}`);
+});
+
+router.get("/logout", (req, res) => {
+    req.logout();
+    req.flash("success", "Goodbye 👋");
     res.redirect("/campgrounds");
 });
 
