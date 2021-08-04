ba68b27e32d78e0bb6306d99e0f02215eba5eaea
Zahi Farah <zahi.farah@protonmail.com>
2021-08-04 11:38:04 +0300

    Require and initialize Multer + implement upload.array (of images) to basic Campground POST route.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware"); // isLoggedIn() middleware
 const catchAsync = require("../utils/catchAsync"); // wrapper function to catch errors and avoid try/catch everywhere
 
+// Multer
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 // controllers: objects that contain methods representing the logic for specific routes
 const campgrounds = require("../controllers/campgrounds");
 
@@ -10,7 +14,11 @@ const campgrounds = require("../controllers/campgrounds");
 
 router.route("/")
     .get(catchAsync(campgrounds.index)) // index
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground)); // new campground
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground)); // new campground
+    .post((upload.array("image")), (req, res) => {
+        console.log(req.body, req.files);
+        res.send("MULTIPASS!")
+    });
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm); /* this should come before the show page,
 otherwise it will be confused by the runtime as an :id */
