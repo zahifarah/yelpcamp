22f36fc1d6978d16953b692cd37adf425278e049
Zahi Farah <zahi.farah@protonmail.com>
2021-08-05 08:40:06 +0300

    Update "image" field to "images" and sgive it two sub parameters "url" and "filename" to better accommodate for Multer/Multer-Storage-Cloudinary/Cloudinary pipeline.

--- models/campground.js
+++ models/campground.js
@@ -5,7 +5,12 @@ const Review = require("./review");
 // schema
 const CampgroundSchema = new mongoose.Schema({
     title: String,
-    image: String,
+    images: [
+        {
+            url: String,
+            filename: String
+        }
+    ],
     price: Number,
     description: String,
     location: String,
