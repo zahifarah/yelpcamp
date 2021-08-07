fec41607dcf09aaf871017c13843daf39fe1b613
Zahi Farah <zahi.farah@protonmail.com>
2021-08-07 16:17:36 +0300

    Add geometry field (mapbox) to model.

--- models/campground.js
+++ models/campground.js
@@ -18,17 +18,28 @@ ImageSchema.virtual("thumbnail").get(function () {
 const CampgroundSchema = new mongoose.Schema({
     title: String,
     images: [ImageSchema],
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
     price: Number,
     description: String,
     location: String,
     author: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
+        ref: 'User'
     },
     reviews: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: "Review"
+            ref: 'Review'
         }
     ]
 });
